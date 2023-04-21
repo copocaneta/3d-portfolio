0dd040a217b00784b17361cf64620845b18fbcd3
Thiago Bernardi <copocaneta@gmail.com>
2023-04-21 18:09:37 -0300

    feat: constant file with all text that will be used

---
+++ src/constants/index.js
@@ -0,0 +1,266 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  crowdform,
+  metabravo,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Fullstack Software Engineer",
+    icon: web,
+  },
+  {
+    title: "React Native Developer",
+    icon: mobile,
+  },
+  {
+    title: "Techlead",
+    icon: backend,
+  },
+  {
+    title: "Master of none",
+    icon: creator,
+  },
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Fullstack engineer",
+    company_name: "Crowdform",
+    icon: metabravo,
+    iconBg: "#E6DEDD",
+    date: "January 2012 - November 2021",
+    points: [
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+    ],
+  },
+  {
+    title: "Fullstack engineer",
+    company_name: "Crowdform",
+    icon: crowdform,
+    iconBg: "#383E56",
+    date: "November 2021 - July 2022",
+    points: [
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+    ],
+  },
+  {
+    title: "Sr Fullstack Lead engineer",
+    company_name: "Crowdform",
+    icon: crowdform,
+    iconBg: "#E6DEDD",
+    date: "July 2022 - November 2022",
+    points: [
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+    ],
+  },
+  {
+    title: "Techlead",
+    company_name: "Crowdform",
+    icon: crowdform,
+    iconBg: "#383E56",
+    date: "November 2022 - current date",
+    points: [
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+      "Lorem ipsum dolor sit amet, qui minim labore adipisicing minim sint cillum sint consectetur cupidatat.",
+    ],
+  },
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "Thiago Bernardi developed some automation processes in our company. Using Python we were able to automate cyclic reporting procedures, monitoring and inventory integration for different platforms, this saved us money and brought us agility in our processes. Some of the robots have been running for two years without requiring intervention. All contractual deadlines and SLA were delivered. I recommend.",
+    name: "Luiz Benine",
+    designation: "CTO",
+    company: "Sarah Chofakian Shoes",
+    image:
+      "https://media.licdn.com/dms/image/D4E03AQE7mG68EiVtvg/profile-displayphoto-shrink_100_100/0/1678157652473?e=1687392000&v=beta&t=-7tYhwRNCLHB8VGMGt55lXo9nkOrKY6W2SD72ZP18io",
+  },
+  {
+    testimonial:
+      "Thiago Bernardi developed some automation processes in our company. Using Python we were able to automate cyclic reporting procedures, monitoring and inventory integration for different platforms, this saved us money and brought us agility in our processes. Some of the robots have been running for two years without requiring intervention. All contractual deadlines and SLA were delivered. I recommend.",
+    name: "Luiz Benine",
+    designation: "CTO",
+    company: "Sarah Chofakian Shoes",
+    image:
+      "https://media.licdn.com/dms/image/D4E03AQE7mG68EiVtvg/profile-displayphoto-shrink_100_100/0/1678157652473?e=1687392000&v=beta&t=-7tYhwRNCLHB8VGMGt55lXo9nkOrKY6W2SD72ZP18io",
+  },
+  {
+    testimonial:
+      "Thiago Bernardi developed some automation processes in our company. Using Python we were able to automate cyclic reporting procedures, monitoring and inventory integration for different platforms, this saved us money and brought us agility in our processes. Some of the robots have been running for two years without requiring intervention. All contractual deadlines and SLA were delivered. I recommend.",
+    name: "Luiz Benine",
+    designation: "CTO",
+    company: "Sarah Chofakian Shoes",
+    image:
+      "https://media.licdn.com/dms/image/D4E03AQE7mG68EiVtvg/profile-displayphoto-shrink_100_100/0/1678157652473?e=1687392000&v=beta&t=-7tYhwRNCLHB8VGMGt55lXo9nkOrKY6W2SD72ZP18io",
+  },
+];
+
+const projects = [
+  {
+    name: "Equalizer Frontend work",
+    description:
+      "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Equalizer Frontend work",
+    description:
+      "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "Equalizer Frontend work",
+    description:
+      "Lorem ipsum dolor sit amet, officia excepteur ex fugiat reprehenderit enim labore culpa sint ad nisi Lorem pariatur mollit ex esse exercitation amet. Nisi anim cupidatat excepteur officia. Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate voluptate dolor minim nulla est proident. Nostrud officia pariatur ut officia. Sit irure elit esse ea nulla sunt ex occaecat reprehenderit commodo officia dolor Lorem duis laboris cupidatat officia voluptate. Culpa proident adipisicing id nulla nisi laboris ex in Lorem sunt duis officia eiusmod. Aliqua reprehenderit commodo ex non excepteur duis sunt velit enim. Voluptate laboris sint cupidatat ullamco ut ea consectetur et est culpa et culpa duis.",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: carrent,
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
