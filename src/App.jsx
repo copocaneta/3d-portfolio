3a599f37dce71c32980e1f161bc88c2c3ca81baa
Thiago Bernardi <copocaneta@gmail.com>
2023-04-21 18:07:42 -0300

    chore: initial setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
+const App = () => {
+  return <div>My new Portfolio website</div>;
+};
+
+export default App;
