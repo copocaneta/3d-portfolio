23c527ee20f4586389b0a781b701e576d42b62a2
Thiago Bernardi <copocaneta@gmail.com>
2023-04-22 00:54:24 -0300

    feat: importing components and files to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  return <div>Navbar</div>;
+};
 
-export default Navbar
+export default Navbar;
