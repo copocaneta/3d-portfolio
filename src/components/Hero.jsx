583a4ac868778c8b24e0390ff453070c68234fc2
Thiago Bernardi <copocaneta@gmail.com>
2023-04-23 20:36:53 -0300

    feat: adding isMobile state and useEffect detector plus animation to invite visitor to scroll down

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { ComputersCanvas } from "./canvas";
 
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <section className="relative w-full h-screen mx-auto">
       <div
@@ -23,7 +36,32 @@ const Hero = () => {
           </p>
         </div>
       </div>
-      <ComputersCanvas />
+      <ComputersCanvas isMobile={isMobile} />
+      <div
+        className={
+          "absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center"
+        }
+      >
+        <a href="#about">
+          <div
+            className={`w-[35px] h-[64px] rounded-3xl  border-4 border-secondary flex justify-center items-start p-2 ${
+              isMobile && "relative top-[100px]"
+            }`}
+          >
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
