41a791dd033ca238ee3bd59d903eaa611e2d6298
Thiago Bernardi <copocaneta@gmail.com>
2023-04-30 20:46:46 -0300

    feat: starting the Tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import { BallCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
+import React from "react";
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech, idx) => {
+        // something
+      })}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionWrapper(Tech, "");
