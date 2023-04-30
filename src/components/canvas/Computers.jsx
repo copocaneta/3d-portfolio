1225703215e533270a96e7c7035b5a2396de1046
Thiago Bernardi <copocaneta@gmail.com>
2023-04-30 20:46:09 -0300

    refactor: experimenting with raytracing and element onclick to detect clicks in 3d gltf model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,27 @@
 import React, { Suspense, useEffect, useState } from "react";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useThree } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
+  const { raycaster } = useThree();
+  // useEffect(() => {
+  //   // if (raycaster.params.Points) {
+  //   //   raycaster.params.Points.threshold = 0.1;
+  //   // }
+  //   if (computer) {
+  //     if (raycaster.intersectObject(computer, true)) {
+  //       // window.open(link1)
+  //       alert("clicou");
+  //     }
+  //   }
+  // }, []);
+  const clickTest = () => {
+    if (raycaster.intersectObject(computer, true)) {
+      // window.open(link1)
+      alert("clicou");
+    }
+  };
   const computer = useGLTF("./copocaneta_setup/copocaneta_setup.gltf");
   return (
     <mesh>
@@ -19,6 +37,11 @@ const Computers = ({ isMobile }) => {
       />
       <primitive
         object={computer.scene}
+        // onPointerDown={(e) =>
+        //   // console.log(`clicked? ${JSON.stringify(e.target)}`)
+        //   clickTest()
+        // }
+        onClick={(e) => console.log("you clicked", e.object.name)}
         scale={isMobile ? 0.6 : 0.95}
         position={isMobile ? [0, -3.5, -1.2] : [0, -4.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
@@ -40,6 +63,10 @@ const ComputersCanvas = ({ isMobile }) => {
           enableZoom={false}
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
+          // minAzimuthAngle={2}
+          // maxAzimuthAngle={1}
+          autoRotate={true}
+          autoRotateSpeed={-0.29}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
