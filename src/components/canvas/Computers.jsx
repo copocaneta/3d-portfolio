6d58b046231f88fdacba4e1b395e74c4e56793f5
Thiago Bernardi <copocaneta@gmail.com>
2023-05-02 01:16:13 -0300

    refactor: playing around with hero section 3d model animation

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,27 +1,51 @@
-import React, { Suspense, useEffect, useState } from "react";
-import { Canvas, useThree } from "@react-three/fiber";
+import React, { Suspense, useEffect, useRef, useState } from "react";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
+import { useSpring, animated } from "@react-spring/three";
+import { Globals } from "@react-spring/shared";
+
+Globals.assign({
+  frameLoop: "always",
+});
 
 const Computers = ({ isMobile }) => {
-  const { raycaster } = useThree();
-  // useEffect(() => {
-  //   // if (raycaster.params.Points) {
-  //   //   raycaster.params.Points.threshold = 0.1;
-  //   // }
-  //   if (computer) {
-  //     if (raycaster.intersectObject(computer, true)) {
-  //       // window.open(link1)
-  //       alert("clicou");
-  //     }
-  //   }
-  // }, []);
-  const clickTest = () => {
-    if (raycaster.intersectObject(computer, true)) {
-      // window.open(link1)
-      alert("clicou");
-    }
-  };
+  // const ref = useRef();
+
+  // useFrame((_, delta) => {
+  //   ref.current.rotation.x += 1 * delta;
+  //   ref.current.rotation.y += 0.5 * delta;
+  // });
+  //
+
+  const { deskRotation } = useSpring({
+    from: {
+      deskRotation: Math.PI / 7,
+    },
+    to: [
+      {
+        deskRotation: -Math.PI / 7,
+      },
+      {
+        deskRotation: Math.PI / 7,
+      },
+      // {
+      //   deskRotation: -1.5 * Math.PI,
+      // },
+      // {
+      //   deskRotation: -2 * Math.PI,
+      // },
+    ],
+    config: {
+      mass: 5,
+      tension: 400,
+      friction: 50,
+      duration: 25000,
+    },
+    loop: true,
+    immediate: true,
+  });
+
   const computer = useGLTF("./copocaneta_setup/copocaneta_setup.gltf");
   return (
     <mesh>
@@ -35,17 +59,20 @@ const Computers = ({ isMobile }) => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive
-        object={computer.scene}
-        // onPointerDown={(e) =>
-        //   // console.log(`clicked? ${JSON.stringify(e.target)}`)
-        //   clickTest()
-        // }
-        onClick={(e) => console.log("you clicked", e.object.name)}
-        scale={isMobile ? 0.6 : 0.95}
-        position={isMobile ? [0, -3.5, -1.2] : [0, -4.25, -1.5]}
-        rotation={[-0.01, -0.2, -0.1]}
-      />
+      <animated.group rotation-y={deskRotation}>
+        <primitive
+          object={computer.scene}
+          // ref={ref}
+          // onPointerDown={(e) =>
+          //   // console.log(`clicked? ${JSON.stringify(e.target)}`)
+          //   clickTest()
+          // }
+          onClick={(e) => console.log("you clicked", e.object.name)}
+          scale={isMobile ? 0.6 : 0.95}
+          position={isMobile ? [0, -3.5, -1.2] : [0, -4.25, -1.5]}
+          rotation={[-0.01, -0.2, -0.1]}
+        />
+      </animated.group>
     </mesh>
   );
 };
@@ -65,8 +92,8 @@ const ComputersCanvas = ({ isMobile }) => {
           minPolarAngle={Math.PI / 2}
           // minAzimuthAngle={2}
           // maxAzimuthAngle={1}
-          autoRotate={true}
-          autoRotateSpeed={-0.29}
+          // autoRotate={true}
+          // autoRotateSpeed={-0.29}
         />
         <Computers isMobile={isMobile} />
       </Suspense>
