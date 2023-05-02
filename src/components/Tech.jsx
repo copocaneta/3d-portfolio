563169ebe2ead670a6604a0ba5c990b3d4b23b5c
Thiago Bernardi <copocaneta@gmail.com>
2023-05-02 01:16:38 -0300

    feat: finishing the Tech section with the 3d balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -7,9 +7,11 @@ import React from "react";
 const Tech = () => {
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
-      {technologies.map((tech, idx) => {
-        // something
-      })}
+      {technologies.map((tech, idx) => (
+        <div className="w-28 h-28 " key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
     </div>
   );
 };
