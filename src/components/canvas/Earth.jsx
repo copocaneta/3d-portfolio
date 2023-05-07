d68a074b6b80a22f616ccb6014fd1f8252b77b7b
Thiago Bernardi <copocaneta@gmail.com>
2023-05-07 20:30:40 -0300

    refactor: changing the orbitControls angle to tilt camera view on 3d model

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -7,7 +7,7 @@ const Earth = () => {
   const lowPolyPlanet = useGLTF("./planet/scene.gltf");
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={0.95} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
         position={[-20, 50, 10]}
@@ -17,14 +17,15 @@ const Earth = () => {
         castShadow
         shadow-mapSize={1024}
       />
-      <primitive
-        object={lowPolyPlanet.scene}
-        // scale={1.2}
-        position-y={0}
-        rotation-y={0}
-        // rotation-x={-362}
-        // rotation-z={5}
+      <spotLight
+        position={[-20, -15, 10]}
+        angle={-0.12}
+        penumbra={1}
+        intensity={0.5}
+        castShadow
+        shadow-mapSize={1024}
       />
+      <primitive object={lowPolyPlanet.scene} position-y={0} rotation-y={0} />
     </mesh>
   );
 };
@@ -38,18 +39,16 @@ const EarthCanvas = () => {
       camera={{
         fov: 45,
         near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
-        rotation: [230, 130, 30],
-        // rotateX: -250,
+        far: 250,
+        position: [-8, 5, 5],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls
           autoRotate
           enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2}
+          maxPolarAngle={Math.PI / 2.25}
+          minPolarAngle={Math.PI / 2.25}
         />
         <Earth />
       </Suspense>
