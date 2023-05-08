00a138f7026451f2699aee9e876adc41b8e2c48e
Thiago Bernardi <copocaneta@gmail.com>
2023-05-07 22:11:14 -0300

    refactor: slideIn fixed

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
   return (
     <div className="xl:mt-12 xl:flex-row flex-col-reverse flex gap-10 overflow-hidden">
       <motion.div
-        variants={slideIn("left", "tween, 0.2, 1")}
+        variants={slideIn("left", "tween", 0.2, 1)}
         className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
       >
         <p className={`${styles.sectionSubText}`}>Get in touch</p>
@@ -30,7 +30,7 @@ const Contact = () => {
             <span>Reach me out on Discord!</span>
           </a>
           <a
-            href="https://discordapp.com/users/724435328823197757/"
+            href="https://t.me/thiago_bernardi"
             target="_blank"
             className="flex flex-row items-center gap-2"
           >
@@ -44,7 +44,7 @@ const Contact = () => {
         </div>
       </motion.div>
       <motion.div
-        variants={slideIn("right", "tween, 0.2, 1")}
+        variants={slideIn("right", "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[250px] h-[350px]"
       >
         <EarthCanvas />
